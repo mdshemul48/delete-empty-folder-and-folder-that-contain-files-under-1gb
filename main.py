d24d5125d86d921763bb5e3a02e264b77ceed17a
MD Shimul <mdshemul480@gmail.com>
2021-07-11 23:30:39 +0600

    getting all the subfolder of subfolder

--- main.py
+++ main.py
@@ -10,8 +10,20 @@ def get_all_folder_dir(root_folder_path: str):
     ]
 
 
+# this will go through all the folder_path and get all those folders sub folder.
+def go_and_get_all_the_sub_folder(root_folder_path):
+    sub_folders_array = []
+
+    for folder_path in root_folder_path:
+        print(folder_path)
+        sub_folders_array += get_all_folder_dir(folder_path)
+
+    return sub_folders_array
+
+
 def main():
     all_sub_folder = get_all_folder_dir(r"A:\.uploading_1tb\check")
+    all_movies_folders = go_and_get_all_the_sub_folder(all_sub_folder)
 
 
 if __name__ == "__main__":
